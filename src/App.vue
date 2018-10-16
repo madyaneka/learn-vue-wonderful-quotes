a91b5e758fd4ee1cf7e23a3e0223be1d25c567f3
Madyan Eka <madyan.eka95@gmail.com>
2018-10-16 14:45:05 +0700

    initializing the application

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="container">
-    
+
     </div>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                quotes: [
+                    'Just a Quote to see something.'
+                ],
+                maxQuotes: 10
+            }
+        }
     }
 </script>
 
